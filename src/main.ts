ef04460a58c8684118f0dfa9ec08f37e674050b8
Pedro Gabriel Queiroz <pedrogabrielqueiroz0207@gmail.com>
2025-05-30 16:34:55 +0000

    atividade feita

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-// Escreva seu código aqui!
+let qtdLuzes: number = 18;
+console.log(qtdLuzes);
+
+let nomeDaProf: string = 'Lanna';
+console.log(nomeDaProf);
+
+let aulaAtiva: boolean = true;
+console.log(aulaAtiva);
+
+let qtdCabos: undefined = undefined;
+console.log(qtdCabos);
+
+let computadorProfessor: { marca: string; ligado: boolean } = {
+  marca: "Dell",
+  ligado: true,
+};
+console.log("Computador da professora:", computadorProfessor)
